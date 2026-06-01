c986e400ac9cac594abd1beddb0180643755ef75
nunezrr <rnunezr@gmail.com>
2026-06-01 00:09:05 -0600

    Probando Actions en Github 12

--- tests/RegistroAsync.spec.ts
+++ tests/RegistroAsync.spec.ts
@@ -21,3 +21,34 @@ test('Transferencia', async ({ page }) => {
   }
 });
 
+test('TransferenciaMultiple', async ({ page }) => {
+  const registro = await new RegistroPageActions(page).open();
+
+  const usuariosPrueba = [
+    { cuenta: '123', monto: '100' },
+    { cuenta: '456', monto: '200' },
+    { cuenta: '789', monto: '300' }
+  ];
+
+  try {
+    for (const usuario of usuariosPrueba) {
+
+      await registro.expectPageLoaded();
+      await registro.isEstadoBoxVisible();
+      await registro.expectEstadoInicial();
+
+      await registro.DatosRegistro(usuario.cuenta, usuario.monto);
+      await registro.clickButton();
+
+      await registro.expectEstadoFinal();
+            
+      await page.reload();
+      console.log("El navegador se ha refrescado correctamente luego de la prueba.", usuario.cuenta);
+    }
+  } catch (error) {
+      console.error("Ocurrió un error durante la ejecución de la transferencia:", error);
+  } finally {
+      await page.close();
+      console.log("El navegador se ha cerrado correctamente luego de la prueba de Transferencia.");
+  }
+});
